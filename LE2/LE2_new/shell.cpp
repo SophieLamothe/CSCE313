e35eb4b60df57e4c0e3dd5d31ecd772290dd2dd4
SophieLamothe <ehtomal19@gmail.com>
2024-09-10 16:51:51 +0000

    Created pipe

--- LE2/LE2_new/shell.cpp
+++ LE2/LE2_new/shell.cpp
@@ -2,6 +2,7 @@
 LE2: Introduction to Unnamed Pipes
 ****************/
 #include <unistd.h> // pipe, fork, dup2, execvp, close
+#include <iostream>
 using namespace std;
 
 int main () {
@@ -12,13 +13,35 @@ int main () {
 
     // TODO: add functionality
     // Create pipe
+    //this array will hold the fild descriptors of the pipe
+    int fd[2]; //pipefd[0] is for reading and pipefd[1] is for writing
 
-    // Create child to run first command
-    // In child, redirect output to write end of pipe
-    // Close the read end of the pipe on the child side.
-    // In child, execute the command
+    //check if pipe creation is successful
+    if(pipe(fd) == -1){
+        // cerr << "Pipe failed" << endl;
+        // return 1; //returning 1 indicates an error
+        perror("Pipe failed");
+        exit(EXIT_FAILURE); 
+    }
 
-    // Create another child to run second command
+    // // Create child to run first command
+    // pid_t firstChildPid = fork(); //creates two processes
+    // if(firstChildPid < 0){
+    //     // cerr << "Fork failed" << endl;
+    //     // return 1; 
+    //     perror("Pipe failed");
+    //     exit(EXIT_FAILURE); 
+    // }
+    // // In child, redirect output to write end of pipe
+    // if(firstChildPid > 0){ //forked child process sucessfully 
+    //     dup2(fd, 1); 
+    
+    // // Close the read end of the pipe on the child side.
+    // close(fd[0]); 
+    // // In child, execute the command
+    // }
+    // // Create another child to run second command
+    // pid_t secondChildPid = fork(); 
     // In child, redirect input to the read end of the pipe
     // Close the write end of the pipe on the child side.
     // Execute the second command.
