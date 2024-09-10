6a5e775c067f3e4e5421ccd907916b725e9b2758
SophieLamothe <ehtomal19@gmail.com>
2024-09-10 17:19:45 +0000

    created child processes, redirect output/input and executed the commands

--- LE2/LE2_new/shell.cpp
+++ LE2/LE2_new/shell.cpp
@@ -25,26 +25,36 @@ int main () {
     }
 
     // // Create child to run first command
-    // pid_t firstChildPid = fork(); //creates two processes
-    // if(firstChildPid < 0){
-    //     // cerr << "Fork failed" << endl;
-    //     // return 1; 
-    //     perror("Pipe failed");
-    //     exit(EXIT_FAILURE); 
-    // }
-    // // In child, redirect output to write end of pipe
-    // if(firstChildPid > 0){ //forked child process sucessfully 
-    //     dup2(fd, 1); 
+    pid_t firstChildPid = fork(); //creates two processes
+    if(firstChildPid < 0){
+        // cerr << "Fork failed" << endl;
+        // return 1; 
+        perror("Fork failed");
+        exit(EXIT_FAILURE); 
+    }
+    // In child, redirect output to write end of pipe
+    if(firstChildPid > 0){ //forked child process sucessfully 
+        dup2(fd[1], 1); 
     
-    // // Close the read end of the pipe on the child side.
-    // close(fd[0]); 
-    // // In child, execute the command
-    // }
-    // // Create another child to run second command
-    // pid_t secondChildPid = fork(); 
-    // In child, redirect input to the read end of the pipe
-    // Close the write end of the pipe on the child side.
-    // Execute the second command.
+        // Close the read end of the pipe on the child side.
+        close(fd[0]); 
+        // In child, execute the command
+        execvp("ls", cmd1); 
+    }
+    // Create another child to run second command
+    pid_t secondChildPid = fork(); 
+    if(secondChildPid < 0){
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
 
+    if(secondChildPid > 0){
+        // In child, redirect input to the read end of the pipe
+        dup2(fd[0], 0); 
+        // Close the write end of the pipe on the child side.
+        close(fd[1]); 
+        // Execute the second command.
+        execvp("tr", cmd2); 
+    }
     // Reset the input and output file descriptors of the parent.
 }
